b8f5ebab1ffc8d90faccb6654fde91a1aa26deb8
glauber2k2 <devglaubermonteiro@gmail.com>
2023-05-16 20:09:37 -0300

    script VoiceToText

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,157 @@
-import { SpeechRecognitionText } from '../SpeechRecognitionText/SpeechRecognitionText';
+import { useEffect, useRef } from 'react';
 
-function App() {
+interface SpeechRecognition extends EventTarget {
+  continuous: boolean;
+  lang: string;
+  onresult: (event: SpeechRecognitionEvent) => void;
+  start(): void;
+  stop(): void;
+}
+
+interface SpeechRecognitionEvent extends Event {
+  readonly resultIndex: number;
+  readonly results: SpeechRecognitionResultList;
+}
+
+interface SpeechRecognitionResultList {
+  [index: number]: SpeechRecognitionResult;
+  length: number;
+}
+
+interface SpeechRecognitionResult {
+  [index: number]: SpeechRecognitionAlternative;
+  length: number;
+}
+
+interface SpeechRecognitionAlternative {
+  transcript: string;
+  confidence: number;
+}
+
+function App(): JSX.Element {
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const btnGravarRef = useRef<HTMLButtonElement>(null);
+  const btnPararRef = useRef<HTMLButtonElement>(null);
+  const btnBaixarRef = useRef<HTMLButtonElement>(null);
+  const btnLimparRef = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    class SpeechApi {
+      speechApi: SpeechRecognition | undefined; // Made speechApi property public
+      private output: HTMLTextAreaElement | null = null; // Marked as optional with default value
+
+      constructor() {
+        const SpeechToText =
+          (window as any).SpeechRecognition ||
+          (window as any).webkitSpeechRecognition;
+
+        if (!SpeechToText) {
+          console.log(
+            'Este navegador não é compatível com o reconhecimento de fala.'
+          );
+          return;
+        }
+
+        this.speechApi = new SpeechToText() as SpeechRecognition;
+        this.output = textareaRef.current!;
+        this.speechApi.continuous = true;
+        this.speechApi.lang = 'pt-BR';
+
+        this.speechApi.onresult = (e) => {
+          const resultIndex = e.resultIndex;
+          const transcript = e.results[resultIndex][0].transcript as string;
+
+          if (this.output) {
+            this.output.value += transcript;
+          }
+        };
+      }
+
+      start() {
+        if (this.speechApi) {
+          this.speechApi.start();
+        }
+      }
+
+      stop() {
+        if (this.speechApi) {
+          this.speechApi.stop();
+        }
+      }
+    }
+
+    const speech = new SpeechApi();
+
+    if (!speech.speechApi) {
+      return;
+    }
+
+    btnGravarRef.current!.addEventListener('click', () => {
+      btnGravarRef.current!.disabled = true;
+      btnPararRef.current!.disabled = false;
+      speech.start();
+    });
+
+    btnPararRef.current!.addEventListener('click', () => {
+      btnGravarRef.current!.disabled = false;
+      btnPararRef.current!.disabled = true;
+      speech.stop();
+    });
+
+    btnBaixarRef.current!.addEventListener('click', () => {
+      const text = textareaRef.current!.value;
+      const filename = 'speech.txt';
+
+      download(text, filename);
+    });
+
+    btnLimparRef.current!.addEventListener('click', () => {
+      textareaRef.current!.value = '';
+      btnGravarRef.current!.disabled = false;
+      btnPararRef.current!.disabled = true;
+      speech.stop();
+    });
+
+    return () => {
+      speech.stop();
+    };
+  }, []);
+  function download(text: string, filename: string) {
+    const element = document.createElement('a');
+
+    element.setAttribute(
+      'href',
+      'data:text/plain;charset=utf-8,' + encodeURIComponent(text)
+    );
+
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+  }
   return (
-    <div>
-      <SpeechRecognitionText />
+    <div className='speech'>
+      <p>RECONHECIMENTO DE VOZ</p>
+      <textarea ref={textareaRef} id='textarea'></textarea>
+      <div>
+        <button ref={btnGravarRef} id='btnGravar'>
+          <i className='fas fa-microphone'>GRAVAR</i>
+        </button>
+        <button ref={btnPararRef} id='btnParar' disabled>
+          <i className='fas fa-microphone-slash'>PARAR</i>
+        </button>
+        <button ref={btnBaixarRef} id='btnBaixar'>
+          <i className='fas fa-download'>BAIXAR</i>
+        </button>
+        <button ref={btnLimparRef} id='btnLimpar'>
+          <i className='fas fa-trash-alt'>LIMPAR</i>
+        </button>
+      </div>
     </div>
   );
 }
